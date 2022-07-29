66d928c9687050c7b6f372c7af9d9c430da49046
Rashmi V Abbigeri <41290852+Rashmi-278@users.noreply.github.com>
2022-07-29 21:48:52 +0530

    Update integrations.tsx

--- src/pages/integrations.tsx
+++ src/pages/integrations.tsx
@@ -2,18 +2,13 @@ import React, { useEffect, useState } from "react";
 import {
   chakra,
   Heading,
-  IconButton,
   Tag,
   Flex,
   useColorModeValue,
-  Button,
-  useBreakpointValue,
   Stack,
   SimpleGrid,
   Center,
 } from "@chakra-ui/react";
-import { ExternalLinkIcon } from "@chakra-ui/icons";
-import Link from "next/link";
 import SimpleNavbar from "../components/SimpleNavbar";
 
 export default function Component() {
@@ -34,12 +29,6 @@ export default function Component() {
   if (!apidata) return <p>No profile data</p>;
   console.log(apidata);
   console.log(typeof apidata);
-  const data = [
-    { name: "Segun Adebayo", email: "sage@chakra.com" },
-    { name: "Josef Nikolas", email: "Josef@mail.com" },
-    { name: "Lazar Nikolov", email: "Lazar@mail.com" },
-    { name: "Abraham", email: "abraham@anu.com" },
-  ];
   const dataColor = useColorModeValue("white", "gray.800");
   const bg = useColorModeValue("white", "gray.800");
   const bg2 = useColorModeValue("gray.200", "gray.700");
@@ -49,7 +38,9 @@ export default function Component() {
       <SimpleNavbar />
       <Center mt={5}>
         {" "}
-        <Heading as='h3' size='lg'>All Live Integrations</Heading>
+        <Heading as="h3" size="lg">
+          All Live Integrations
+        </Heading>
       </Center>
       <Flex
         w="full"
@@ -64,6 +55,22 @@ export default function Component() {
           bg={{ md: bg }}
           shadow="lg"
         >
+          <SimpleGrid
+            columns={{ base: 1, md: 4 }}
+            w={{ base: "initial", md: "full" }}
+            textTransform="uppercase"
+            bg={bg2}
+            color={"gray.500"}
+            py={{ base: 1, md: 4 }}
+            px={{ base: 2, md: 8 }}
+            fontSize="md"
+            fontWeight="hairline"
+          >
+            <span>Name</span>
+            <chakra.span>Category</chakra.span>
+            <chakra.span>Type</chakra.span>
+            <chakra.span>Status</chakra.span>
+          </SimpleGrid>
           {apidata.map((ele) => {
             return (
               <Flex
@@ -72,36 +79,15 @@ export default function Component() {
                 key={ele.cname}
               >
                 <SimpleGrid
-                  spacingY={3}
                   columns={{ base: 1, md: 4 }}
-                  w={{ base: 200, md: "full" }}
-                  textTransform="uppercase"
-                  bg={bg2}
-                  color={"gray.500"}
-                  py={{ base: 1, md: 4 }}
-                  px={{ base: 2, md: 10 }}
-                  fontSize="md"
-                  fontWeight="hairline"
-                >
-                  <span>Name</span>
-                  <chakra.span>Category</chakra.span>
-                  <chakra.span>Type</chakra.span>
-                  <chakra.span>Status</chakra.span>
-              
-                </SimpleGrid>
-                <SimpleGrid
-                  spacingY={3}
-                  columns={{ base: 1, md: 4 }}
-                  w="full"
+                  w="-moz-fit-content"
                   py={2}
                   px={10}
-                  fontWeight="hairline"
+                  fontWeight="normal"
                 >
                   <span>{ele.name}</span>
                   <span>{ele.categories}</span>
                   <span>{ele.type}</span>
-
-                  
 
                   <chakra.span textOverflow="ellipsis" overflow="hidden">
                     {ele.isEnabled ? (
